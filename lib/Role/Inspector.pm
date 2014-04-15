aca08ca493360eb5d2864f23bcf4a901fba537ae
Toby Inkster <mail@tobyinkster.co.uk>
2014-04-15 10:22:25 +0100

    bump version; changelog

--- lib/Role/Inspector.pm
+++ lib/Role/Inspector.pm
@@ -5,7 +5,7 @@ use warnings;
 package Role::Inspector;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.003';
+our $VERSION   = '0.004';
 
 use Exporter::Shiny qw( get_role_info learn );
 use Module::Runtime qw( use_package_optimistically );
